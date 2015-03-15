79209531d0990bfb982fe12739eaac3e4842faa5
Rugoals <rugoals@gmail.com>
2015-03-15 10:51:28 +0300

    update

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <?php
 
-<<<<<<< HEAD
 $debug = 1;
-=======
-$debug = 0;
->>>>>>> 5506eb3dd2665c5b30cc3bfc7559ff15bfa6a02b
 if($debug > 0) {
 	ini_set('error_reporting', E_ALL);
 	ini_set ('display_errors', 1);
 }
 
-<<<<<<< HEAD
 
 define("PATH_MODXTRANSFER", $_SERVER['DOCUMENT_ROOT'].'/assets/modules/transfer/'); 
 
@@ -32,22 +27,11 @@ $options = array(
   4 => 'Парсинг текста',
   5 => 'Парсинг документа'
 );
-=======
-define("PATH_MODXTRANSFER", $_SERVER['DOCUMENT_ROOT'].'/modx_transfer/'); 
-
-require_once(PATH_MODXTRANSFER.'model/modx_transfer.class.php');
-
-$modxtransfer = new modx_transfer($config);
->>>>>>> 5506eb3dd2665c5b30cc3bfc7559ff15bfa6a02b
  
 ?>
 
 <!DOCTYPE html>
-<<<<<<< HEAD
 <html lang="ru">
-=======
-<html lang="en">
->>>>>>> 5506eb3dd2665c5b30cc3bfc7559ff15bfa6a02b
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -59,11 +43,7 @@ $modxtransfer = new modx_transfer($config);
     <title>Перенос на MODX</title>
 
     <!-- Bootstrap core CSS -->
-<<<<<<< HEAD
     <link href="/assets/modules/transfer/template/css/bootstrap.min.css" rel="stylesheet">
-=======
-    <link href="modx_transfer/template/css/bootstrap.min.css" rel="stylesheet">
->>>>>>> 5506eb3dd2665c5b30cc3bfc7559ff15bfa6a02b
  
   </head>
 
@@ -78,7 +58,6 @@ $modxtransfer = new modx_transfer($config);
       <h3>Выберите опцию:</h3>
 
 <p>
-<<<<<<< HEAD
 <form action="" method="post">
 
   <select name="option" id="">
@@ -99,20 +78,12 @@ $modxtransfer = new modx_transfer($config);
 
 </form>
 
-=======
-	<select name="" id="">
-		<option value=""></option>
-		<option value="1">Показать ссылки</option>
-		<option value="2">Перенести контент</option>
-	</select>
->>>>>>> 5506eb3dd2665c5b30cc3bfc7559ff15bfa6a02b
 </p>
 
 <hr />
 
 <?
 
-<<<<<<< HEAD
 
 
 if($option == 1) {
@@ -127,22 +98,11 @@ if($option == 1) {
  echo $transfer->update_content_from_document();
 }
 
-=======
-echo $modxtransfer->set_redirect_url();
->>>>>>> 5506eb3dd2665c5b30cc3bfc7559ff15bfa6a02b
 
 ?>
    <hr />  
    
     </div> <!-- /container -->
-<<<<<<< HEAD
  
    </body>
-=======
-
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
-  </body>
->>>>>>> 5506eb3dd2665c5b30cc3bfc7559ff15bfa6a02b
 </html>
